bda9db1f5bcc4880ccabe39265e270fc3f84dec8
nomthandazo <ndlovunomthandazo4@gmail.com>
2024-06-07 00:17:53 +0200

    Ddestroy APIViews

--- todos/serializers.py
+++ todos/serializers.py
@@ -5,4 +5,4 @@ class TodoSerializer(ModelSerializer):
 
     class Meta:
         model=Todo
-        fields = ('title', 'desc', 'is_complete',)
+        fields = ('id','title', 'desc', 'is_complete',)
