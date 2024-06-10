3dfcf3a330f2a848390faebccbd2f76bafe4f56f
nomthandazo <ndlovunomthandazo4@gmail.com>
2024-06-10 11:34:31 +0200

    TestTodo

--- todos/urls.py
+++ todos/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", TodosAPIView.as_view(), name="todos"),
-    path("<int:id>", TodoDetailAPIView.as_view(), name="todos")
+    path("<int:id>", TodoDetailAPIView.as_view(), name="todo"),
 
 ]
